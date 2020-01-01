d99e60d39c9aa3186f33ff6ff1e3721faa67e425
Meghan Stovall <meghanstovall15@augustana.edu>
2020-01-01 13:27:32 -0700

    Finish iteration 2 for prework

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -1,1 +1,39 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/customer'
+require './lib/pet'
 
+class CustomerTest < Minitest::Test
+
+  def setup
+    @joel = Customer.new("Joel", 2)
+    @samson = Pet.new({name: "Samson", type: :dog})
+    @lucy = Pet.new({name: "Lucy", type: :cat})
+  end
+
+  def test_it_exists
+    assert_instance_of Customer, @joel
+  end
+
+  def test_it_has_attributes
+    assert_equal "Joel", @joel.name
+    assert_equal 2, @joel.id
+    assert_equal [], @joel.pets
+  end
+
+  def test_can_adopt_pets
+    @joel.adopt(@samson)
+    @joel.adopt(@lucy)
+
+    assert_equal [@samson, @lucy], @joel.pets
+  end
+
+  def test_outstanding_balance
+    assert_equal 0, @joel.outstanding_balance
+
+    @joel.charge(15)
+    @joel.charge(7)
+
+    assert_equal 22, @joel.outstanding_balance
+  end
+end
