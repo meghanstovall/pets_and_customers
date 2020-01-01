7676b8e9293ad20b6c419a6825b3a31061081fcb
Meghan Stovall <meghanstovall15@augustana.edu>
2020-01-01 13:09:07 -0700

    Initial commit (starting over)

--- lib/pet.rb
+++ lib/pet.rb
@@ -1,18 +1,1 @@
-class Pet
-  attr_reader :name,
-              :type
 
-  def initialize(attributes)
-    @name = attributes[:name]
-    @type = attributes[:type]
-    @fed = false
-  end
-
-  def feed
-    @fed = true
-  end
-
-  def fed?
-    @fed
-  end
-end
